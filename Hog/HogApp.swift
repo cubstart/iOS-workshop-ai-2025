7369184aa11781558787c5eddd323dadee3375b6
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-06-14 23:52:26 -0700

    Initial Commit

---
+++ Hog/HogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HogApp.swift
+//  Hog
+//
+//  Created by Justin Wong on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
