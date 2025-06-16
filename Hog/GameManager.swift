a4d9603642355c369e21a6e92b0a40f266fd35cd
Chhumbucket <dylchhum@gmail.com>
2025-06-16 01:10:22 -0700

    Need to add a nil unwrapping and implement startTurn

--- Hog/GameManager.swift
+++ Hog/GameManager.swift
@@ -6,9 +6,10 @@
 //
 import SwiftUI
 
+//Maybe add a nil and unwrapping 
 @Observable
 class GameManager {
-    var players: [Player] = [Player(playerID: 1), Player(playerID: 2)]
+    var players: [Player] = [Player(playerID: 1, isTurn: true), Player(playerID: 2)]
     var dices: [Dice] = []
     var currentPlayerIndex: Int = 0
     var isGameOver: Bool = false
@@ -24,11 +25,20 @@ class GameManager {
     }
     
     func resetGame() {
-        players = [Player(playerID: 1), Player(playerID: 2)]
+        players = [Player(playerID: 1, isTurn: true), Player(playerID: 2)]
         currentPlayerIndex = 0
         isGameOver = false
         winner = nil
         dices.removeAll()
+    }
+    
+    //Where to add?
+    func startTurn() {
+        if currentPlayer.hitOne {
+            var curr = players[currentPlayerIndex]
+            curr.points += currentPlayer.tempPoints
+            curr.hitOne = false
+        }
     }
     
     func endTurn() {
@@ -64,19 +74,24 @@ class GameManager {
     
     private func calcScore() -> Int {
         var sum = 0
+        var hasOne = false
         
         //Game punishes for hitting ones so maybe fix later
         for dice in dices {
             let value = Int(dice.value.rawValue) ?? 0
             if value == 1 {
-                return 1
+                hasOne = true
+                sum += 1
             } else if value % 3 == 0 || value % 4 == 0 {
                 return nextPrime(after: currentPlayer.points)
             } else {
                 sum += value
             }
         }
-        return sum
+        if hasOne {
+            players[currentPlayerIndex].tempPoints = sum
+        }
+        return hasOne ? 1 : sum
     }
     
     
--- Hog/PlayerModel.swift
+++ Hog/PlayerModel.swift
@@ -8,4 +8,7 @@
 struct Player {
     let playerID : Int
     var points : Int = 0
+    var tempPoints : Int = 0
+    var hitOne : Bool = false
+    var isTurn : Bool = false
 }
